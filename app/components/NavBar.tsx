f4487f11d09fd18e4640571e5e3212708f41454a
HimanshuSinghRaghav <hsinghraghav1@gmail.com>
2022-12-02 18:43:56 +0530

    Feature page added and Navbar maked responsive

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,47 +1,158 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Icon from './Images/icon.png'
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Icon from "./Images/icon.png";
+import { MODERN_BROWSERSLIST_TARGET } from "next/dist/shared/lib/constants";
+
 const Landing = () => {
+  const [nevScroll, setNavScroll] = useState(false);
+  // Mobile View
+  const [mobileView, setMobileView] = useState(false);
+
+  const changeNavScroll = () => {
+    if (window.scrollY >= 60) {
+      setNavScroll(true);
+    } else {
+      setNavScroll(false);
+    }
+  };
+
+  const mobileSlideBar = () => {
+    mobileView ? setMobileView(false) : setMobileView(true);
+  };
+
+  window.addEventListener("scroll", changeNavScroll);
+
   return (
     <>
-     <div className='bg-[#fbf3ec] py-6 w-screen sticky'>
-         <nav className='space-x-96'>
-            <div>
-              
-              <span className='flex ml-5 mb-3 absolute text-black'>
-              <Image
-              src={Icon}
-              alt="icon"
-              width={40}
-              height={40}
-              />
-              <Link className='mt-1' href={"#"}>
+      <div
+        className={`justify-between  w-full fixed scroll-smooth ${
+          nevScroll ? "bg-[#fbf3ec] shadow-lg py-6" : "bg-transparent py-12"
+        } duration-500`}
+      >
+        <nav className="space-x-[150px] flex">
+          <div>
+            <span className="flex ml-6 mb-9 absolute text-black">
+              <Image src={Icon} alt="icon" width={40} height={40} />
+              <Link className="mt-1" href={"#"}>
                 Streamway
               </Link>
-              </span>
+            </span>
+          </div>
 
+          <div className="pl-56 hidden sm:block">
+            <ul className="flex space-x-10 pt-2 text-black ">
+              <Link className="hover:text-red-700  space-x-2" href={"#"}>
+                <div className="flex">
+                  Home
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 mt-1 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </div>
+              </Link>
+              <Link className="hover:text-red-700" href={"#"}>
+                Porifolio
+              </Link>
+              <Link className="hover:text-red-700" href={"#"}>
+                Pages
+              </Link>
+              <Link className="hover:text-red-700" href={"#"}>
+                Blogs
+              </Link>
+              <Link className="hover:text-red-700" href={"#"}>
+                Docs
+              </Link>
+            </ul>
+          </div>
 
+          <div className="space-x-4 sm:flex hidden">
+            <Link href={"#"} className="font-bold mt-2">
+              Login
+            </Link>
+            <div className="relative w-40 h-10 group bg-[#655c97] text-white">
+              <div className="absolute bg-[#973c56]  z-10 left-0 w-0 h-10  group-hover:w-full   duration-700"></div>
+              <button className="absolute  w-40 h-10 bg-transparent z-20 ">
+                Create Your Event
+              </button>
             </div>
+          </div>
+        </nav>
 
-            <div>
-               <ul  className='flex space-x-10    text-black '>
-                <Link className='hover:text-red-700 flex space-x-2' href={"#"}>Home
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mt-1 ">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-</svg>
-
-                </Link>
-                <Link className='hover:text-red-700' href={"#"}>Porifolio</Link>
-                <Link className='hover:text-red-700' href={"#"}>Pages</Link>
-                <Link className='hover:text-red-700' href={"#"}>Blogs</Link>
-                <Link className='hover:text-red-700' href={"#"}>Docs</Link>
-               </ul>
-            </div>
-         </nav>
-     </div>
+        {/* Mobile  View */}
+        <div className=" mt-1 md:hidden">
+          <div className="flex flex-row-reverse mr-36">
+            <svg
+              onClick={() => {
+                mobileSlideBar();
+              }}
+              className="w-8 h-8 stroke-white bg-[#973c56] rounded-md "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </div>
+          <div
+            className={`w-screen h-[340px] bg-[#fbf3ec] pt-5 ${
+              mobileView ? "block" : "hidden "
+            }`}
+          >
+            <ul className="divide-y-[1px] divide-opacity-10 divide-gray-400 space-y-4 ">
+              <li></li>
+              <li>
+                <p className="ml-4 hover:text-text-red-700 mt-2">Home</p>
+              </li>
+              <li>
+                <p className="ml-4 hover:text-text-red-700 mt-2">Porifolio</p>
+              </li>
+              <li>
+                <p className="ml-4 hover:text-text-red-700 mt-2">Pages</p>
+              </li>
+              <li>
+                <p className="ml-4 hover:text-text-red-700 mt-2">Blog</p>
+              </li>
+              <li>
+                <p className="ml-4 hover:text-text-red-700 mt-2">Dose</p>
+              </li>
+              {/* <li ><p className='ml-4'>Home</p></li> */}
+              <li className="grid place-items-center">
+                <div className="space-x-4 flex mt-5">
+                  <Link href={"#"} className="font-bold mt-2">
+                    Login
+                  </Link>
+                  <div className="relative w-40 h-10 group bg-[#655c97] text-white">
+                    <div className="absolute bg-[#973c56]  z-10 left-0 w-0 h-10  group-hover:w-full   duration-700"></div>
+                    <button className="absolute  w-40 h-10 bg-transparent z-20 ">
+                      Create Your Event
+                    </button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Feature from './Feature'
 const Main = () => {
   return (
     <div className='bg-[#fbf3ec] w-screen h-full'>
-      <div className='w-[80%] h-[100%] pt-[275px] flex ml-48 '>
+      <div className='w-[80%] h-[100%] pt-[220px] flex ml-48 '>
         <div className='w-[50%]'>
-            <p className='text-8xl'>Event ticket organiser & seller.</p>
+            <p className='text-7xl'>Event ticket organiser & seller.</p>
             <p className='text-3xl py-12'>For hassale free event, we are here to help you by creating online ticket.</p>
         </div>
         <div className='w-[50%]'>
@@ -16,6 +16,20 @@ const Main = () => {
         </div>
 
       </div>
+
+      <div className="pl-44 mt-10 flex space-x-[500px]">
+        <div className="relative w-52 h-12 group bg-[#655c97] text-white">
+          <div className="absolute bg-[#973c56]  z-10 left-0 w-0 h-12  group-hover:w-full   duration-700"></div>
+          <button className="absolute  w-44 h-12 bg-transparent z-20 text-xl">
+            Let's get started
+          </button>
+        </div>
+        <p className="text-2xl">
+        Over <span className="text-3xl text-red-800 underline">150,000+ client</span> all over the world.
+        </p>
+      </div>
+      <Feature/>
+      <img className="bg-white" src="https://html.creativegigstf.com/deski/deski/images/shape/99.svg" alt="" />
     </div>
   )
 }
