299641a1230d73b4a8ece1cd09693c09b0056fc6
Shubham Kansara <63890928+Shivaaykansara@users.noreply.github.com>
2022-12-14 16:59:20 +0530

    Call By Action With Images

--- app/components/CAB.tsx
+++ app/components/CAB.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-
+import Im1 from "./Images/210.svg"
+import Im2 from "./Images/211.svg"
+import Image from "next/image";
 const CAB = () => {
   return (
-    <div className="container relative mx-auto bg-no-repeat p-10 md:p-16 pt-0 bg-cover bg-center ">
+    <div className="container my-52 bg-[#f5fbff] relative z-10 mx-auto bg-no-repeat p-10 md:p-16 pt-0 bg-cover bg-center ">
       
-        <img
-        src="https://deskireact.ibthemespro.com/images/shape/211.svg"
+        <Image
+        src={Im2}
         className="absolute -z-10 w-[15%] left-0 top-0"
+        alt="nothing"
+        ></Image>
+        <Image
+        src={Im1}
+        className="absolute -z-10 w-[15%] bottom-0 right-0"
         alt=""
-        />
-        <img
-        src="https://deskireact.ibthemespro.com/images/shape/210.svg"
-        className="absolute w-[15%] bottom-0 right-0"
-        alt=""
-        />
-      <div className="text-center pt-5 md:p-16 ">
+        ></Image>
+      <div className="container text-center pt-5 md:p-16 z-10">
         <div>
           <span className="f text-4xl md:text-6xl font-semibold relative tracking-wider inline-block">
             Try Streamway Free
@@ -31,7 +33,7 @@ const CAB = () => {
           </span>
         
       </div>
-      <div className="container w-fit md:w-[80%] mx-auto md:flex justify-center mt-5 md:mt-0 space-y-2 md:space-y-0 ">
+      <div className="container w-fit md:w-[80%] text-center mx-auto md:flex mt-5 md:mt-0 space-y-2 md:space-y-0 ">
           <input
             type="tel"
             className="md:w-[400px] border-[3px] inline-block mr-2 text-black text-lg md:text-xl p-1 md:p-5 border-black rounded-3xl"
@@ -39,7 +41,7 @@ const CAB = () => {
           />
         <button
           type="submit"
-          className="text-white ml-10 sm:ml-0 bg-[#fd6a5e] border-[3px] p-2 md:px-5 md:text-2xl tracking-tighter font-semibold  rounded-full hover:border-[#fd6a5e] hover:bg-white hover:text-[#fd6a5e]"
+          className="text-white bg-[#fd6a5e] border-[3px] p-2 md:px-5 md:text-2xl tracking-tighter font-semibold  rounded-full hover:border-[#fd6a5e] hover:bg-white hover:text-[#fd6a5e]"
         >
           Boost My Views
         </button>
