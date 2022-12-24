db0c05faf3926417e79587e1535a301f4595438d
Shubham Kansara <63890928+Shivaaykansara@users.noreply.github.com>
2022-12-24 16:59:22 +0530

    Blog section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
+      'tb': '900px',
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
