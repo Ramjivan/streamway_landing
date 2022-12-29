8e970fc43f605263678264a2bac6ef8c6d94a0b8
Surender deora <surender82905@gmail.com>
2022-12-29 18:42:13 +0530

    faq

---
+++ pages/Faq-question-answer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+ const Faq_question_answer = 
+ [
+        {
+            id:1,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+        {
+            id:2,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+        {
+            id:3,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+        {
+            id:4,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+        {
+            id:5,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+        {
+            id:6,
+            question:'How does the free trial work?',
+            answer:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque perferendis commodi illo quia corporis cupiditate exercitationem rem unde minus accusantium, doloribus inventore mollitia soluta repudiandae nesciunt placeat itaque adipisci. Odit vitae laboriosam, mollitia, sit quis aliquid numquam commodi cum dignissimos laborum exercitationem voluptas minus placeat sequi distinctio architecto atque soluta sunt odio iusto quod.',
+        },
+    ]
+
+
+
+export default Faq_question_answer;
--- pages/Faq.tsx
+++ pages/Faq.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Dropdown from './images/dropdown.png'
+import Faq_question_answer from './Faq-question-answer'
 
 const Faq = () => {
+  const [data, setData] = useState(Faq_question_answer);
+  const [show, setShow] = useState(false)
   return (
     <div className='container mx-auto'>
       <div>
         <p className='roboto text-xl text-[#8A8A8A] tracking-widest text-center uppercase pb-2 leading-[28px]'>FAQ</p>
         <h1 className='text-[58px] text-[#101621] text-center capitalize font-bold pb-2 leading-[28px]'>Question & Answer</h1>
+        <div className='py-10'>
+          {/*  faq */}
+          {
+            data.map((curElement) => {
+              const { id, question, answer } = curElement;
+              return (
+
+                <div className='max-w-[900px] mx-auto my-10'>
+                  <h2 onClick={() => setShow(!show)} className='cursor-pointer shadow-lg shadow-cyan-500/50  text-2xl text-left py-8 px-7 relative'>
+                    {curElement.question}
+                    <Image src={Dropdown} alt="img" className={`inline-block w-[25px] absolute right-[3%] top-[40%] ${show ? `rotate-180` : `rotate-0`}`} />
+                  </h2>
+                  {
+                    show && <p className='shadow-lg shadow-cyan-500/50 duration-1000 text-2xl text-left pt-3 pb-8 px-7'> {curElement.answer} </p>
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+
       </div>
     </div>
   )
