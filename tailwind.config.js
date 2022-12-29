091aba25f67e9681c1111787cfc30d6b6095b135
Shubham Kansara <63890928+Shivaaykansara@users.noreply.github.com>
2022-12-29 17:07:02 +0530

    animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,13 +31,10 @@ module.exports = {
         'animation-rotatee-rev': 'rotateerev 60s linear infinite',
         'animation-bouncii': 'bouncee 4.5s linear infinite',
         'animation-bouncii-rev': 'bouncerev 4.5s linear infinite',
-<<<<<<< HEAD
         'animation-rotatee': 'rotatee 60s linear infinite',
         'animation-rotatee-rev': 'rotateerev 60s linear infinite',
         'marquee': 'marquee 20s linear infinite',
         'marquee2': 'marquee2 20s linear infinite'
-=======
->>>>>>> c91fe97fcea77c6201f9b295b287a7a035abc721
       }
     },
   },
