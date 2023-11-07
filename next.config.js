5479c2285a11ee4f2d8efa2343310d93547c33f5
HimanshusinghRaghav <hsinghraghav1@gmail.com>
2023-11-07 17:49:54 +0530

    Update Firebase configuration and build settings

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output:'export',
+  images : {
+    unoptimized: true,
+  },
+  distDir: 'build',
+  
 }
 
 module.exports = nextConfig
